dbe0924763827e7c955d42a96b12756191e4f2dc
Daseul.Kim <da91love@gmail.com>
2024-01-02 10:03:50 +0900

    [update]create Slack class done

--- common/const/API_URL.py
+++ common/const/API_URL.py
@@ -1,1 +1,2 @@
-SLACK_URL = 'https://boosta.boosters-labs.com/slack/send'
+SLACK_ADD_POST_URL = 'https://boosta.boosters-labs.com/slack/send'
+SLACK_UPDATE_POST_URL = 'https://https://mna.boosters-labs.com/slack/update'
