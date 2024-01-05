6f7f81e18b11beb7dc1c20023a1e4e9f0cac59eb
Daseul.Kim <da91love@gmail.com>
2024-01-05 10:28:56 +0900

    [update]modify mail body

--- api_gmail_sender/const/mail_info.py
+++ api_gmail_sender/const/mail_info.py
@@ -1,22 +1,25 @@
-mail_subject = '🏊‍♂️Dive into Radiant Skin: Partnering with Eqqualberry for a Paid Glow-Up!🔥'
+mail_subject = '🌟Elevate Your Glow: Special Collaboration Offer with Eqqualberry Toner💙'
 mail_body = '''
 <div>Hi {},</div>
-<br />
-<div>I hope this message finds you well😀<span>&nbsp;</span><span>&nbsp;</span>My name is Anna, and I represent Eqqualberry, a Korean Skincare brand focusing on balanced skincare with sufficient depth and comfortable usability, focusing on the essence and role of the products used every day.</div>
-<div>I recently came across your incredible TikTok account and couldn't help but be captivated by your content. Your genuine passion for beauty products truly shines through, and it's exactly that authenticity that caught our eye.</div>
-<div><br/>We are thrilled to introduce you to our star product, the Eqqualberry Swimming Pool Toner &ndash; a skincare gem that we believe will become a staple in your routine. Crafted with care, this toner boasts the power of KERASOLAZE for delicate exfoliation and a blend of five antioxidant-rich berries along with hyaluronic acid to provide deep hydration. It's perfect for sensitive, dry, or combination skin, and the results are simply radiant!<span>&nbsp;</span></div>
-<span>&nbsp;</span>
+<br/>
+<div>I trust this message finds you in good spirits. My name is Anna, and I represent Eqqualberry, a distinguished Korean Skincare brand that places a strong emphasis on balanced skincare, delving into the depth of quality and user comfort. Your remarkable TikTok account recently caught our eye, and we couldn't help but be captivated by your genuine passion for beauty products. 🌈</div>
+<br/>
+<div>We're thrilled to introduce you to our standout product, the Eqqualberry Swimming Pool Toner—a skincare gem meticulously crafted with the power of Protease for delicate exfoliation and a blend of five antioxidant-rich berries paired with hyaluronic acid for deep hydration. Designed for sensitive, dry, or combination skin, the results are nothing short of radiant. ✨</div>
+<br/>
 <div><b>🍓Key Benefits:</b></div>
 <ul>
-<li>Delicate exfoliation with KERASOLAZE</li>
+<li>Delicate exfoliation with Protease, improving acne scars (dark spots)</li>
 <li>Deep hydration with five berry extracts and hyaluronic acid</li>
 <li>Refines pores and reduces blackheads</li>
 <li>Ideal for sensitive and dry/combination skin</li>
 </ul>
-<div>Our slogan says it all: "Experience Radiance: For Skin Care That's Healthy and Balanced!" We believe this aligns perfectly with your beauty philosophy and the kind of content your followers would love.</div>
-<div><br/>We'd love to collaborate with you on a TikTok post featuring our Swimming Pool Toner. To express our appreciation for your creativity and influence, <b>we're more than happy to discuss a reasonable compensation.</b> Please let me know if you are interested in!</div>
-<div><br/>Looking forward to the possibility of creating something interesting together!🩷<span>&nbsp;</span></div>
-<div>Warmest regards,<br/>Anna</div>
+<div>Our slogan, "Experience Radiance: For Skin Care That's Healthy and Balanced," aligns seamlessly with your beauty philosophy, reflecting the authenticity and depth that resonate with your followers.</div>
+<br/>
+<div>We're excited about the prospect of collaborating with you on a TikTok post featuring our Swimming Pool Toner. In recognition of your creativity and influence, we're more than happy to discuss fair compensation. 💖</div>
+<div>Please let us know if this opportunity resonates with you. Looking forward to the possibility of creating something remarkable together!</div>
+<br/>
+<div>Warmest regards, 🌷Anna</div>
+
 <span>&nbsp;</span>
 <div>Tiktok: @eqqualberry_us</div>
 <div>Instagram: @eqqualberry_us</div>
