7400d47c372cf5c4f452bd9cc48f2d29ba058115
Daseul.Kim <da91love@gmail.com>
2024-01-17 14:29:39 +0900

    [update]send directly not through make

--- common/lib/ma/data_access/system/AccessService.py
+++ common/lib/ma/data_access/system/AccessService.py
@@ -9,6 +9,19 @@ class AccessService(AccessServiceBase):
     All function's name should start with below 4 verbs: select insert update delete
     """
     @staticmethod
+    def select_infl_first_contact(**bindings):
+        """
+        :param bindings: (tuple)
+        :return: (list) sql query result
+        """
+        try:
+            return AccessServiceBase.execute_sql(
+                sql=Query.sql_select_infl_first_contact,
+                bindings=bindings)
+
+        except Exception as e:
+            raise e
+    @staticmethod
     def select_delivery_history(**bindings):
         """
         :param bindings: (tuple)
