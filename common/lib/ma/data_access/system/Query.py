19daaa34a86f7bdd971d6c96da378d4e21e16aa0
daseul.kim <da91love@gmail.com>
2024-01-23 02:42:10 +0900

    [update]status check in 10min

--- common/lib/ma/data_access/system/Query.py
+++ common/lib/ma/data_access/system/Query.py
@@ -56,7 +56,7 @@ class Query():
     sql_select_status_in_x_min = """
         SELECT *
         FROM contact_status
-        WHERE TIMESTAMPDIFF(MINUTE, created_at, NOW()) <= 60;
+        WHERE TIMESTAMPDIFF(MINUTE, created_at, NOW()) <= 10;
     """
 
     sql_select_pic = """
