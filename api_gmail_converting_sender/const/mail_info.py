95dc8ec9ba1f7a6e182774acfc99f5a0c46f749d
daseul.kim <da91love@gmail.com>
2024-01-28 17:40:53 +0900

    [update]done

--- api_gmail_converting_sender/const/mail_info.py
+++ api_gmail_converting_sender/const/mail_info.py
@@ -1,16 +1,11 @@
-mail_subject = '🌟Update from the Eqqualberry PR team!!💙'
+mail_subject = 'Update from the Eqqualberry PR team!!🌟'
 mail_body = '''
 <div>Hello, {},</div>
 <br/>
-<div>Nice to e-meet you 🌈</div>
+<div>I am Jennifer! How is you weekend? 🌈</div>
 <br/>
-<div>My name is Jennifer, part of the Eqqualberry PR team. I'm also a colleague of Anna. ✨</div>
-<div>Anna may be difficult to reach for a while, so I will be taking over the communication.</div>
-<br/>
-<div>If there was an ongoing discussion, I would very appreciate it if you resend it once to this email address.</div>
-<div>If you have any inquiries or concerns regarding our collaboration, please feel free to reach out to this address.</div>
-<br/>
-<div>Thank you for your wide understanding.😉</div>
+<div>If you have any update, please follow up this email!</div>
+<div>Thank you for your help😉</div>
 <br/>
 <div>Warmest regards, 🌷Jennifer</div>
 
