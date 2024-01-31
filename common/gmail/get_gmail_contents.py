67173a88407d0e33455669f90e76997d3c75e442
Daseul.Kim <da91love@gmail.com>
2024-01-31 15:24:48 +0900

    [update]이스케이브 '

--- common/gmail/get_gmail_contents.py
+++ common/gmail/get_gmail_contents.py
@@ -3,13 +3,13 @@ from common.util.StrUtil import StrUtil
 
 def get_gmail_contents(mail_thread_obj):
     try:
-    #     encode_data_of_mail_body = mail_thread_obj.get('payload').get('parts')[0].get('body').get('data')
-    #
-    #     if encode_data_of_mail_body:
-    #         decoded = EncodingUtil.encode_byte_to_str(encode_data_of_mail_body)
-    #         return StrUtil.clean_mail_body(decoded)
-    #     else:
-        return mail_thread_obj.get('snippet')
+        encode_data_of_mail_body = mail_thread_obj.get('payload').get('parts')[0].get('body').get('data')
+
+        if encode_data_of_mail_body:
+            decoded = EncodingUtil.encode_byte_to_str(encode_data_of_mail_body)
+            return StrUtil.clean_mail_body(decoded)
+        else:
+            return mail_thread_obj.get('snippet')
     except Exception as e:
         raise e
 
