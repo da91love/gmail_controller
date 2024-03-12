b512f2494b23aae8eec1a580786b15978e5472f2
Daseul.Kim <da91love@gmail.com>
2024-03-12 17:05:38 +0900

    [update]escape single quatation'

--- common/util/StrUtil.py
+++ common/util/StrUtil.py
@@ -1,11 +1,14 @@
 import re
 
-
 class StrUtil:
     @staticmethod
     def clean_mail_body(text):
         # Use regular expression to remove lines starting with ">"
         remove_quote = re.sub(r'^>.*$', '', text, flags=re.MULTILINE)
         remove = remove_quote.replace('\r', '').replace('\n', '')
-        escaped_value = remove.replace("'", "''")
-        return escaped_value
+        # escaped_value = remove.replace("'", "''")
+        return remove
+
+    @staticmethod
+    def is_html(content):
+        return any(tag in content for tag in ("<html>", "<body>", "<div>", "<p>", "<a>"))
