ca1de9708554119b03ea403e2951b9df5c9ab76a
Daseul.Kim <da91love@gmail.com>
2024-04-02 15:59:25 +0900

    [update]parameter

--- api_gmail_sender/app.py
+++ api_gmail_sender/app.py
@@ -108,6 +108,5 @@ def app_api_gmail_sender(event, context=None):
 
     return ResType(data=sent_done_tg).get_response()
 
-# result = app_api_gmail_sender(None)
-# print(result)
-
+result = app_api_gmail_sender(None)
+print(result)
