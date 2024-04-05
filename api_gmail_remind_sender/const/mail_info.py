accc8808e3f967443a1ceedb373b6a7e0b284a51
Daseul.Kim <da91love@gmail.com>
2024-04-05 17:13:36 +0900

    [update]modify msg

--- api_gmail_remind_sender/const/mail_info.py
+++ api_gmail_remind_sender/const/mail_info.py
@@ -1,38 +1,12 @@
 mail_info = [{
-        "mail_subject": "Paid Collaboration with Eqqualberry's TikTok ",
+        "mail_subject": "Paid Collaboration with Branden's TikTok ",
         "mail_body": '''
-            <div>HI! I am Jennifer😄 I hope you had a chance to review our previous invitation to join our TikTok Seeding Program. We believe that your influence in the skincare community can greatly benefit from this collaboration.</div>
+            <div>HI! I am Grace😄 I hope you had a chance to review our previous invitation to join our TikTok Seeding Program. We believe that your influence in Tiktok can greatly benefit from this collaboration.</div>
             <br/>
-            <div>This is a last reminder to join our program. Don't miss out on the opportunity to showcase our latest skincare products to your followers and be a part of our exclusive influencer community.</div>
+            <div>This is a last reminder to join our program. Don't miss out the opportunity to showcase our packing cube products to your followers and be a part of our exclusive influencer community.</div>
             <br/>
             <div>If you have any questions or need more information, feel free to reach out. We value your partnership and look forward to hearing from you soon!</div>
-            <div>Best Regard, Jennifer</div>
-        '''
-    },
-    {
-        "mail_subject": "Exciting Opportunity: Proposal for a Eqqualberry's Paid Collaboration💸!",
-        "mail_body": '''
-            <div>HI! I am Jennifer😄 We wanted to reach out one more time to offer you exclusive access to our upcoming skincare product launches through our TikTok Seeding Program.</div>
-            <br/>
-            <div>By participating, you'll be among the first to experience and share these products with your followers, gaining early access before they hit the market.</div>
-            <br/>
-            <div>If you are interested in this exclusive opportunity, please let us know at your earliest convenience. We can't wait to see your creativity in action!</div>
-            <div>Best Regard, 🌷Jennifer</div>
-        '''
-    },
-    {
-        "mail_subject": "Last Call to Join Eqqualberry's TikTok Paid Collaboration!🤑",
-        "mail_body": '''
-            <div>Hello, I am Jennifer😄 </div>
-            <br/>
-            <div>This is the final call to join our TikTok Seeding Program! We have been eagerly anticipating your response and would love to have you as part of our exclusive group of influencers.</div>
-            <br/>
-            <div>The deadline is approaching, and we don't want you to miss out on this exciting opportunity. If you're ready to take your TikTok content to the next level and showcase our skincare products, now is the time to let us know.</div>
-            <div>We appreciate your time and consideration. Looking forward to having you on board!🥲 </div>
-            <br/>
-            <div>Thank you🙃 </div>
-            <br/>
-            <div>Best regard, 🌷Jennifer</div>
+            <div>Best Regard, Grace</div>
         '''
     }
 ]
--- common/gmail/EmailMsgCreator.py
+++ common/gmail/EmailMsgCreator.py
@@ -9,65 +9,56 @@ class EmailMsgCreator:
             body = f'''
                 <div>Hi {author_unique_id},</div>
                 <br/>
-                <div>This is Jennifer, I’m a brand manager here in Eqqualberry.</div>
-                <div><b>We’re a Korean skincare brand</b>, and we at Eqqualberry believe in a strong emphasis on balanced skincare, we are committed to creating clean beauty that is all natural and free from harsh chemicals!</div>
+                <div>This is Grace, I’m a brand manager here in Branden.</div>
+                <div>I’m reaching out to you with an exciting collaboration opportunity that I believe aligns perfectly with your channel and your audience’s interests.</div>
+                <div>Our slogan, “TRAVELING BECOMES EASIER,” aligns seamlessly with your content, reflecting the authenticity and depth that resonate with you and your audience.</div>
                 <br/>
-                <div>Your TikTok channel recently caught our eye, and we couldn’t help but be captivated by your genuine passion for beauty products.</div>
+                <div><b>Branden is currently the #1 best-selling compression packing cube in Korea😊 </b> Currently, we’ve reached 4100,000 in sales with 35,000 of 5stars reviews in the market.</div>
                 <br/>
-                <div>Recently, we went viral and were amazed by how much attention @charinecheungg’s collaboration with us has gotten. It’s not just about the number of views but we were able to see that many TikTok viewers were genuinely interested and are in need of our product. As a result, <b>Eqqualberry’s Swimming Pool Toner is currently ranked 14th overall for toner on Amazon US.</b></div>
+                <div>Many Korean celebrities and mega-creators have also collaborated with us as they really loved our product after using it.</div>
                 <br/>
-                <div>We are excited to share with you that we have <b>recently launched our product on Shopee</b> and this will be available across the Southeast Asian market as well, and <b>we’d love to collaborate with you as one of the first few creators to introduce our product to your audience!😉</b></div>
+                <div>For several years, we’ve extensively focused on developing compression packing cubes. We pride ourselves with the quality of our products compared to any other competitors out in the market and we’ve also received so many positive reviews and feedback from our consumers as it’s so easy for us.</b></div>
                 <br/>
-                <div>To provide you with a clearer picture, please see the following key benefits on our toner!</div>
+                <div>To help you better understand our product’s unique compression feature, kindly check out our official global tiktok channel below.</div>
                 <br/>
-                <div><b>* Key Benefits:</b></div>
-                <ul>
-                <li>Ideal for sensitive and reactive skin</li>
-                <li>Contain Protease to gently exfoliate skin and keep pores clean</li>
-                <li>Deep hydration with five berry extracts and hyaluronic acid</li>
-                <li>Refines pores and reduces blackheads</li>
-                <li>Fungal acne and acne safe</li>
-                </ul>
-                <div>Please check out <b>"#eqqualberrySwimmingPoolToner"</b> in Amazon for product details.</div>
+                <div>With our product’s unique compression feature and your creative touch, we believe we can create even more compelling and genuine content together. If you’re interested in exploring this collaboration further, we’d love to discuss next steps at your earliest convenience. Please let us know your thoughts and looking forward to speaking with you.</div>
                 <br/>
-                <div>We truly looking forward to potentially collaborating with you, and if you are interested we’d love to speak next steps soon!</div>
-                <br/>
-                <div>Warmest regards, Jennifer</div>
+                <div>Warmest regards, Grace</div>
                 
                 <span>&nbsp;</span>
-                <div>Tiktok: @eqqualberry_us</div>
-                <div>Instagram: eqqualberry_us</div>
+                <div>Tiktok: @branden_official</div>
+                <div>Instagram: branden.seoul</div>
             '''
         elif seeding_num == 2:
-            subject = 'Re-Collaboration offer with Eqqualberry'
+            subject = 'Re-Collaboration offer with Branden'
             body = f'''
                 <div>Hi {author_unique_id},</div>
-                <div>I'm Jennifer from Eqqualberry.</div>
+                <div>I'm Grace from Branden.</div>
                 <div>The previous video you posted was really great! I appreciate and respect it a lot.</div>
                 <div>So I'd like to suggest a second collaboration.</div>
                 <br/>
                 <div>If you have an interest, Let's discuss next step of collaboration through this email thread!</div>
-                <div>Warmest regards, Jennifer</div>
+                <div>Warmest regards, Grace</div>
 
                 <span>&nbsp;</span>
-                <div>Tiktok: @eqqualberry_us</div>
-                <div>Instagram: eqqualberry_us</div>
+                <div>Tiktok: @branden_official</div>
+                <div>Instagram: branden.seoul</div>
             '''
 
         elif seeding_num == 3:
             subject = 'Re-Collaboration offer with Eqqualberry'
             body = f'''
                 <div>Hi {author_unique_id},</div>
-                <div>I'm Jennifer from Eqqualberry.</div>
+                <div>I'm Grace from Branden.</div>
                 <div>The previous video you posted was really great! I appreciate and respect it a lot.</div>
-                <div>So I'd like to suggest a third collaboration.</div>
+                <div>So I'd like to suggest a second collaboration.</div>
                 <br/>
                 <div>If you have an interest, Let's discuss next step of collaboration through this email thread!</div>
-                <div>Warmest regards, Jennifer</div>
+                <div>Warmest regards, Grace</div>
 
                 <span>&nbsp;</span>
-                <div>Tiktok: @eqqualberry_us</div>
-                <div>Instagram: eqqualberry_us</div>
+                <div>Tiktok: @branden_official</div>
+                <div>Instagram: branden.seoul</div>
             '''
 
         return {
