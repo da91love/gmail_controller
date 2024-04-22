65eeb7f2857468295cf8fa372d1e8e86e39979e7
Daseul.Kim <da91love@gmail.com>
2024-04-22 11:01:15 +0900

    [update]change send msg

--- common/gmail/EmailMsgCreator.py
+++ common/gmail/EmailMsgCreator.py
@@ -14,13 +14,13 @@ class EmailMsgCreator:
                 <div>Our slogan, “TRAVELING BECOMES EASIER,” aligns seamlessly with your content, reflecting the authenticity and depth that resonate with you and your audience.</div>
                 <br/>
                 <div><b>Branden is currently the #1 best-selling compression packing cube in Korea😊 </b> </div>
-                <div>We’ve reached 400,000 in sales with 35,000 of 5stars reviews in the market, Also many Korean celebrities and mega-creators have collaborated with us</div>
-                <div>And finally, <b>We’re planning to launch in Amazon US this coming April!</b></div>
+                <div>We’ve reached 400,000 in sales with 200,000 of 5stars reviews in the market, Also many Korean celebrities and mega-creators have collaborated with us</div>
                 <br/>
                 <div>For several years, we’ve extensively focused on developing compression packing cubes. </div>
-                <div/We pride ourselves with the quality of our products compared to any other competitors out in the market and we’ve also received so many positive reviews and feedback from our consumers as it’s so easy for us.</b></div>
+                <div>We pride ourselves with the quality of our products compared to any other competitors out in the market and we’ve also received so many positive reviews and feedback from our consumers as it’s so easy for us.</div>
                 <br/>
                 <div>To help you better understand our product’s unique compression feature, kindly check out our official global tiktok channel below.</div>
+                <div>Or search <b>branden packing cube</b> on Amazon.</div>
                 <br/>
                 <div>With our product’s unique compression feature and your creative touch, we believe we can create even more compelling and genuine content together. </div>
                 <div>If you’re interested in exploring this collaboration further, we’d love to discuss next steps at your earliest convenience. </div>
@@ -29,7 +29,7 @@ class EmailMsgCreator:
                 <div>Warmest regards, Grace</div>
                 
                 <span>&nbsp;</span>
-                <div><b>Tiktok: @branden_official</b></div>
+                <div><b>Tiktok: @branden_global</b></div>
                 <div><b>Instagram: branden.seoul</b></div>
             '''
         elif seeding_num == 2:
@@ -44,8 +44,8 @@ class EmailMsgCreator:
                 <div>Warmest regards, Grace</div>
 
                 <span>&nbsp;</span>
-                <div>Tiktok: @branden_official</div>
-                <div>Instagram: branden.seoul</div>
+                <div><b>Tiktok: @branden_global</b></div>
+                <div><b>Instagram: branden.seoul</b></div>
             '''
 
         elif seeding_num == 3:
