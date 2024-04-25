608f92111b8b5a8711c1a96c5ca73b69b8dc6fe4
Daseul.Kim <da91love@gmail.com>
2024-04-25 12:05:17 +0900

    [update]eqb sender email

--- common/const/EMAIL.py
+++ common/const/EMAIL.py
@@ -1,2 +1,3 @@
+SENDER_EMAILS = ['eqqualberry.comm@boosters.kr','eqqualberry.comm2@boosters.kr','eqqualberry.comm3@boosters.kr','eqqualberry.comm4@boosters.kr','eqqualberry.comm5@boosters.kr']
 INBOX = 'INBOX'
 SENT = 'SENT'
