9a83a7a4f3690f0bbab71e248255401a40a9ac39
Daseul.Kim <da91love@gmail.com>
2024-06-26 18:03:56 +0900

    [update]change config

--- config/development.py
+++ config/development.py
@@ -6,7 +6,7 @@ config = {
     'DB': {
             'mysql': {
                 'db_host': '13.125.110.215',
-                'db_database': 'brdn_us',
+                'db_database': 'eqb_us',
                 'db_port': '3306',
                 'db_user': 'mna',
                 'db_password': 'mna11!!',
