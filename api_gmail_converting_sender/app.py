e079b84e858c1c20de0b37ec00b0d69653e38f64
Daseul.Kim <da91love@gmail.com>
2024-06-27 10:11:57 +0900

    [update]add

--- api_gmail_converting_sender/app.py
+++ api_gmail_converting_sender/app.py
@@ -107,6 +107,6 @@ def app_api_gmail_converting_sender(event, context=None):
 
     return ResType(data=sent_done_tg).get_response()
 
-result = app_api_gmail_sender(None)
-print(result)
+# result = app_api_gmail_sender(None)
+# print(result)
 
