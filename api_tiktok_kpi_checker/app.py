bf17c177fb675b50c480a6b68dbc9ae2a07c1bbb
Daseul.Kim <da91love@gmail.com>
2024-07-08 18:04:05 +0900

    [update]resolve confliction

--- api_tiktok_kpi_checker/app.py
+++ api_tiktok_kpi_checker/app.py
@@ -126,6 +126,6 @@ def api_tiktok_kpi_checker(event, context=None):
 
     return ResType(data={}).get_response()
 
-result = app_api_kpi_checker(None)
+result = api_tiktok_kpi_checker(None)
 print(result)
 
