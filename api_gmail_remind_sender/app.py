f608508fe800df520bca94b000c15111bb596202
Daseul.Kim <da91love@gmail.com>
2024-07-19 11:11:54 +0900

    [update]not remind mail to add to sys

--- api_gmail_remind_sender/app.py
+++ api_gmail_remind_sender/app.py
@@ -58,8 +58,8 @@ def app_api_gmail_remind_sender(event, context=None):
     remind_mails = []
     remind_mails_num = 0
     for latest_sent_contact in latest_sent_contacts:
-        gmail_thread_id, gmail_msg_id, receiver_email, sender_email, t_key, status, progress, pic, created_at \
-            = itemgetter('gmail_thread_id', 'gmail_msg_id', 'receiver_email', 'sender_email', 't_key', 'status', 'progress','pic', 'created_at')(latest_sent_contact)
+        gmail_thread_id, gmail_msg_id, receiver_email, sender_email, add_to_sys, t_key, status, progress, pic, created_at \
+            = itemgetter('gmail_thread_id', 'gmail_msg_id', 'receiver_email', 'sender_email', 'add_to_sys', 't_key', 'status', 'progress','pic', 'created_at')(latest_sent_contact)
 
         is_cnt_bf = True if cntc_num_grouped_by_tkey[t_key][0]['thread_count'] > 1 else False
         sent_num = len(cnts_grouped_by_tid[gmail_thread_id])
@@ -67,63 +67,65 @@ def app_api_gmail_remind_sender(event, context=None):
         # declare instance
         labelControl = LabelControl(sender_email)
 
-        # remind 메일 송신은 300회로 한정
-        if remind_mails_num < 150:
-            # 이메일 전환으로 이미 보낸 이력이 있을 경우 remind 보내지 않음
-            if not is_cnt_bf:
-                # only send remind status open
-                if status == STATUS['OPEN']:
-                    # if sent number is over 5, no remind send
-                    if sent_num < 2:
-                        # remind if over 5 days # TODO: change days diff
-                        day_diff = (datetime.now() - created_at).days
-                        if day_diff > 5:
-                            # Extract the information you need, e.g., sender, receiver, mail_subject, etc.
-                            mail_subject = mail_info[sent_num - 1]['mail_subject']
-                            mail_body = mail_info[sent_num - 1]['mail_body']
-                            formatted_datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        # add to sys 가 false
+        if add_to_sys == False:
+            # remind 메일 송신은 300회로 한정
+            if remind_mails_num < 150:
+                # 이메일 전환으로 이미 보낸 이력이 있을 경우 remind 보내지 않음
+                if not is_cnt_bf:
+                    # only send remind status open
+                    if status == STATUS['OPEN']:
+                        # if sent number is over 5, no remind send
+                        if sent_num < 2:
+                            # remind if over 5 days # TODO: change days diff
+                            day_diff = (datetime.now() - created_at).days
+                            if day_diff > 5:
+                                # Extract the information you need, e.g., sender, receiver, mail_subject, etc.
+                                mail_subject = mail_info[sent_num - 1]['mail_subject']
+                                mail_body = mail_info[sent_num - 1]['mail_body']
+                                formatted_datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-                            # Send the message
-                            # 이전 스레드가 존재할시 기존 스레드에 엮어서 보내고
-                            try:
-                                sent_message = send_re_email(sender_email, receiver_email, mail_subject, mail_body, gmail_thread_id)
+                                # Send the message
+                                # 이전 스레드가 존재할시 기존 스레드에 엮어서 보내고
+                                try:
+                                    sent_message = send_re_email(sender_email, receiver_email, mail_subject, mail_body, gmail_thread_id)
 
-                                # insert to contact db
-                                AccessService.insert_contact_history(
-                                    gmail_thread_id=gmail_thread_id,
-                                    gmail_msg_id=sent_message.get('id'),
-                                    gmail_label_id='SENT',
-                                    t_key=t_key,
-                                    created_at=formatted_datetime
-                                )
-                            # 기존 스레드 존재하지 않아 에러 발생 시 새로운 메일로 송신
-                            except HttpError as e:
-                                sent_message = send_email(sender_email, receiver_email, mail_subject, mail_body)
+                                    # insert to contact db
+                                    AccessService.insert_contact_history(
+                                        gmail_thread_id=gmail_thread_id,
+                                        gmail_msg_id=sent_message.get('id'),
+                                        gmail_label_id='SENT',
+                                        t_key=t_key,
+                                        created_at=formatted_datetime
+                                    )
+                                # 기존 스레드 존재하지 않아 에러 발생 시 새로운 메일로 송신
+                                except HttpError as e:
+                                    sent_message = send_email(sender_email, receiver_email, mail_subject, mail_body)
 
-                                # prepare variables
-                                new_gmail_thread_id = sent_message.get("threadId")
-                                gmail_msg_id = sent_message.get("id")
+                                    # prepare variables
+                                    new_gmail_thread_id = sent_message.get("threadId")
+                                    gmail_msg_id = sent_message.get("id")
 
-                                # modify label
-                                labelControl.add_label(gmail_msg_id=gmail_msg_id, add_label_names=[status, progress, pic])
+                                    # modify label
+                                    labelControl.add_label(gmail_msg_id=gmail_msg_id, add_label_names=[status, progress, pic])
 
-                                # update contact
-                                AccessService.update_gmail_mail_contact_thread_id(
-                                    new_gmail_thread_id=new_gmail_thread_id,
-                                    old_gmail_thread_id=gmail_thread_id
-                                )
+                                    # update contact
+                                    AccessService.update_gmail_mail_contact_thread_id(
+                                        new_gmail_thread_id=new_gmail_thread_id,
+                                        old_gmail_thread_id=gmail_thread_id
+                                    )
 
-                                # insert to contact db
-                                AccessService.insert_contact_history(
-                                    gmail_thread_id=new_gmail_thread_id,
-                                    gmail_msg_id=gmail_msg_id,
-                                    gmail_label_id='SENT',
-                                    t_key=t_key,
-                                    created_at=formatted_datetime
-                                )
+                                    # insert to contact db
+                                    AccessService.insert_contact_history(
+                                        gmail_thread_id=new_gmail_thread_id,
+                                        gmail_msg_id=gmail_msg_id,
+                                        gmail_label_id='SENT',
+                                        t_key=t_key,
+                                        created_at=formatted_datetime
+                                    )
 
-                            remind_mails.append(sent_message)
-                            remind_mails_num += 1
+                                remind_mails.append(sent_message)
+                                remind_mails_num += 1
 
     return ResType(data=remind_mails).get_response()
 
