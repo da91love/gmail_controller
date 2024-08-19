63a8f59a51e0096656015b68d65e3982cf216741
Daseul.Kim <da91love@gmail.com>
2024-08-19 14:19:50 +0900

    [update]spark ads tgt on tiktok trk

--- common/lib/ma/data_access/system/AccessService.py
+++ common/lib/ma/data_access/system/AccessService.py
@@ -8,6 +8,21 @@ class AccessService(AccessServiceBase):
     """
     All function's name should start with below 4 verbs: select insert update delete
     """
+
+    @staticmethod
+    def select_spark_ads_trk_tg(**bindings):
+        """
+        :param bindings: (tuple)
+        :return: (list) sql query result
+        """
+        try:
+            return AccessServiceBase.execute_sql(
+                sql=Query.sql_select_spark_ads_trk_tg,
+                bindings=bindings)
+
+        except Exception as e:
+            raise e
+
     @staticmethod
     def insert_clm_posting_history(**bindings):
         """
--- get_post_stat_as_multithread.py
+++ get_post_stat_as_multithread.py
@@ -23,36 +23,26 @@ if __name__ == "__main__":
     tg_posts_info = []
     posts_info = AccessService.select_post_info()
     posts_history_grouped_by_id = _.group_by(AccessService.select_posting_history(), 'post_id')
+
+    # 시딩 포스트 중 트래킹 대상 선정
     for post_info in posts_info:
         # 1차 필터링: post_type 비디오만 취득
         post_type = post_info['post_type']
         if post_type == 'video':
-
-            # 2차 필터링: spark ads 여부 확인
-            spark_ads_start_date = post_info['spark_ads_start_date']
-
-            if spark_ads_start_date:
-                # sparks ads 유통기한 확인
-                spark_ads_end_date = post_info['spark_ads_end_date']
-                day_diff = (spark_ads_end_date - datetime.now()).days
-
-                if day_diff >= 0:
+            post_history = posts_history_grouped_by_id.get(post_info['post_id'])
+            # 2차 필터링 : post history 1개 라도 존재하면 3차필터링으로, 아니면 대상 추가
+            if post_history:
+                posted_time = post_history[0]['posted_time']
+                day_diff = (datetime.now() - posted_time).days
+                # 3차 필터링: 게시날이 7일 이내일 것
+                if day_diff <= 14:
                     tg_posts_info.append(post_info)
-
             else:
-                post_history = posts_history_grouped_by_id.get(post_info['post_id'])
+                tg_posts_info.append(post_info)
 
-                # 2차 필터링 : post history 1개 라도 존재하면 3차필터링으로, 아니면 대상 추가
-                if post_history:
-                    posted_time = post_history[0]['posted_time']
-                    day_diff = (datetime.now() - posted_time).days
-                    # 3차 필터링: 게시날이 7일 이내일 것
-                    if day_diff <= 14:
-                        tg_posts_info.append(post_info)
-                else:
-                    tg_posts_info.append(post_info)
-
-
+    # 스파크에즈 포스트 중 트래킹 대상 선정
+    spark_ads_post_info = AccessService.select_spark_ads_trk_tg()
+    tg_posts_info += spark_ads_post_info
 
     tg_posts_id = [i['post_id'] for i in tg_posts_info]
 
