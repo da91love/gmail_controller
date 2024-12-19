66fd84721ea0d234575dc76eea3170f672f61f34
Daseul.Kim <da91love@gmail.com>
2024-12-19 11:24:04 +0900

    [update]add alert new buyer api

--- api_listener.py
+++ api_listener.py
@@ -40,6 +40,8 @@ def alert_new_buyer_post():
     body = request.json
     headers = request.headers
 
+    print(body)
+
     result = alert_new_buyer(body)
 
     return make_response(jsonify(result))
