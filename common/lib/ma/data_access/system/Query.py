8fb4691356c8b77e72e9f293dfddc2ca058af978
Daseul.Kim <da91love@gmail.com>
2025-02-19 15:19:09 +0900

    [update] 슬랙에 2개 안올라오는 문제 해결 중

--- common/lib/ma/data_access/system/Query.py
+++ common/lib/ma/data_access/system/Query.py
@@ -19,7 +19,7 @@ class Query():
 
     sql_insert_slack_history = """
         insert into slack_thread_history (export_id, slack_post_block_id, pic, buyer_name, address, recipient, recipient_phone_num, requested_arrival_date, bill_type, folder_id, delivery, delivery_type, remark) 
-        values ('{export_id}', '{slack_post_block_id}', '{pic}',　'{buyer_name}','{address}','{recipient}','{recipient_phone_num}','{requested_arrival_date}','{bill_type}','{folder_id}','{delivery}','{delivery_type}','{remark}')
+        values ('{export_id}','{slack_post_block_id}','{pic}','{buyer_name}','{address}','{recipient}','{recipient_phone_num}','{requested_arrival_date}','{bill_type}','{folder_id}','{delivery}','{delivery_type}','{remark}')
     """
 
     sql_select_slack_history = """
