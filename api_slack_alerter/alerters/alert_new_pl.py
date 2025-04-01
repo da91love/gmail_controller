f5b609a19ea4f03fbccb89c91f9bdfdcb0de5be8
Daseul.Kim <da91love@gmail.com>
2025-04-01 16:26:05 +0900

    [update]change slack channel id

--- api_slack_alerter/alerters/alert_new_pl.py
+++ api_slack_alerter/alerters/alert_new_pl.py
@@ -35,7 +35,7 @@ def alert_new_pl(data):
 
             # 업데이트 문구 리플라이
             slack.add_reply(
-                channel_id=SLACK_GLOBAL_B2B_DELIVERY_REQUEST_ID,
+                channel_id=SLACK_GLOBAL_B2B_INVOICE_ID,
                 msg_type=MSG_TYPE['BLOCK'],
                 msg_body=SlackMsgCreator.get_slack_new_doc_reply_block(requester=requester, doc_type=doc_type),
                 thread_ts=slack_block_id
