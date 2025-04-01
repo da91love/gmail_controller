975e1c3e31c3d4889faf6e8902b41c6664769041
Daseul.Kim <da91love@gmail.com>
2025-04-01 16:22:56 +0900

    [update] deliveryType

--- api_slack_alerter/alerters/alert_new_ci.py
+++ api_slack_alerter/alerters/alert_new_ci.py
@@ -16,7 +16,7 @@ def alert_new_ci(data):
             invoices = data[dt]
 
             # get value from payload
-            export_no, delivery_type, requester = itemgetter('exportNo','requester','deliveryType')(invoices[0])
+            export_no, requester, delivery_type = itemgetter('exportNo','requester','deliveryType')(invoices[0])
             # define doc type
             doc_type = CI
 
